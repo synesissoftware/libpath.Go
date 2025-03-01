4077c751fb5113557cfb95f02c91aed2fce8c814
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-03-01 17:48:26 +1100

    ~ boilerplate

--- version.go
+++ version.go
@@ -1,3 +1,12 @@
+// Copyright 2019-2025, Matthew Wilson and Synesis Information Systems. All
+// rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+ * Created: 25th February 2025
+ * Updated: 1st March 2025
+ */
+
 package libpath
 
 import "github.com/synesissoftware/ver2go"
@@ -6,7 +15,7 @@ const (
 	VersionMajor uint16 = 0
 	VersionMinor uint16 = 0
 	VersionPatch uint16 = 0
-	VersionAB    uint16 = 0x0003
+	VersionAB    uint16 = 0x0004
 	Version      uint64 = (uint64(VersionMajor) << 48) + (uint64(VersionMinor) << 32) + (uint64(VersionPatch) << 16) + (uint64(VersionAB) << 0)
 )
 
